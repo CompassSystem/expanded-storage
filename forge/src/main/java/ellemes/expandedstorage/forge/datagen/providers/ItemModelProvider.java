50a29177b3224413d5fde2236c1001fbac48b65c
Ellemes <3llemes@gmail.com>
2022-11-12 18:07:12 +0000

    Pull in recent changes from 1.19.2 branch

--- forge/src/main/java/ellemes/expandedstorage/forge/datagen/providers/ItemModelProvider.java
+++ forge/src/main/java/ellemes/expandedstorage/forge/datagen/providers/ItemModelProvider.java
@@ -4,7 +4,6 @@ import ellemes.expandedstorage.common.misc.Utils;
 import ellemes.expandedstorage.common.datagen.providers.ModelHelper;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.client.model.generators.ModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
