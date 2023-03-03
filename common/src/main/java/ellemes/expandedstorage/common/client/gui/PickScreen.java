366c7b036037981c25790b695cfa168abd4966e7
Ellemes <3llemes@gmail.com>
2023-03-03 18:30:43 +0000

    Remove deadcode from pickscreen.

--- common/src/main/java/ellemes/expandedstorage/common/client/gui/PickScreen.java
+++ common/src/main/java/ellemes/expandedstorage/common/client/gui/PickScreen.java
@@ -17,7 +17,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
@@ -31,29 +30,23 @@ public final class PickScreen extends Screen {
     public static final Map<ResourceLocation, PickButton> BUTTON_SETTINGS = new HashMap<>();
     private final Set<ResourceLocation> options = ImmutableSortedSet.copyOf(PickScreen.BUTTON_SETTINGS.keySet());
     private final Supplier<Screen> returnToScreen;
-    private final @NotNull Runnable onOptionPicked;
     private final AbstractHandler handler;
     private int topPadding;
 
     public PickScreen(AbstractScreen currentScreen) {
         this(currentScreen.getMenu(), () -> {
             return AbstractScreen.createScreen(currentScreen.getMenu(), Minecraft.getInstance().player.getInventory(), currentScreen.getTitle());
-        }, () -> {});
+        });
     }
 
     public PickScreen(Supplier<Screen> returnToScreen) {
-        this(null, returnToScreen, () -> {});
+        this(null, returnToScreen);
     }
 
-    public PickScreen(@NotNull Runnable onOptionPicked) {
-        this(null, () -> null, onOptionPicked);
-    }
-
-    private PickScreen(@Nullable AbstractHandler handler, Supplier<Screen> returnToScreen, Runnable onOptionPicked) {
+    private PickScreen(@Nullable AbstractHandler handler, Supplier<Screen> returnToScreen) {
         super(Component.translatable("screen.ellemes_container_lib.screen_picker_title"));
         this.handler = handler;
         this.returnToScreen = returnToScreen;
-        this.onOptionPicked = onOptionPicked;
     }
 
     @Deprecated
@@ -118,7 +111,6 @@ public final class PickScreen extends Screen {
     private void updatePlayerPreference(ResourceLocation selection) {
         PlatformHelper.instance().clientHelper().configWrapper().setPreferredScreenType(selection);
         this.onClose();
-        onOptionPicked.run();
     }
 
     @Override
